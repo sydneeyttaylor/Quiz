0341f5dde2c53861f9469ffcee698dad107b1b56
Sydney Boratto <sboratto@venom.highpoint.edu>
2018-01-15 15:39:03 -0500

    Initial Commit

---
+++ Quiz1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz1
+//
+//  Created by Sydney Boratto on 1/15/18.
+//  Copyright © 2018 Sydney Boratto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

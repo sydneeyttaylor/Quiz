96315921638da9b2485740c43799e7acd19ea7c8
Sydney Boratto <sboratto@i143>
2018-01-21 18:08:40 -0500

    added comment and label for name to autograph app

--- Quiz1/ViewController.swift
+++ Quiz1/ViewController.swift
@@ -3,23 +3,56 @@
 //  Quiz1
 //
 //  Created by Sydney Boratto on 1/15/18.
+//  High Point University
 //  Copyright © 2018 Sydney Boratto. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
 
+    let answers: [String] = [
+       "14",
+       "Montpelier",
+       "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    
+    
+    @IBAction func showNextQuestion(_sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count{
+            currentQuestionIndex = 0
+        }
+    
+
+       let question: String = questions[currentQuestionIndex]
+       questionLabel.text = question
+       answerLabel.text = "???"
+    }
+        
+    @IBAction func showAnswer(_sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+ }
+  
 
 
-}
 
